de9db5c2acb265e11dbfe0d328316506bfaeba48
frasnym <frastyawan.nym@gmail.com>
2021-01-15 11:33:11 +0800

    Ensure AuthRouter

---
+++ src/presentation/routers/auth.router.spec.js
@@ -0,0 +1,37 @@
+class AuthRouter {
+  route (httpRequest) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+describe('Auth Router', () => {
+  test('should return 400 if no email is provided', () => {
+    const authRouter = new AuthRouter()
+
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = authRouter.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('should return 400 if no password is provided', () => {
+    const authRouter = new AuthRouter()
+
+    const httpRequest = {
+      body: {
+        email: 'any@email.com'
+      }
+    }
+
+    const httpResponse = authRouter.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
