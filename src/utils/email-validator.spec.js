f760ba99026252cefa58f609333d4b506fb1464c
frasnym <frastyawan.nym@gmail.com>
2021-01-15 16:26:29 +0800

    Ensure EmailValidator returns false if validation returns false

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,6 +1,8 @@
+const validator = require('validator')
+
 class EmailValidator {
   isValid (email) {
-    return true
+    return validator.isEmail(email)
   }
 }
 
@@ -11,5 +13,14 @@ describe('Email Validator', () => {
     const isEmailValid = emailValidator.isValid('valid_email@mail.com')
 
     expect(isEmailValid).toBe(true)
+  })
+
+  test('should return false if validator returns false', () => {
+    validator.isEmailValid = false
+    const emailValidator = new EmailValidator()
+
+    const isEmailValid = emailValidator.isValid('invalid_email@mail.com')
+
+    expect(isEmailValid).toBe(false)
   })
 })
