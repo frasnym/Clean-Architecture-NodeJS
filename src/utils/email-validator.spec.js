eb9a0f97bda8de3e74b6452f36dd3e361e46a7bb
frasnym <frastyawan.nym@gmail.com>
2021-01-15 16:30:26 +0800

    Ensure EmailValidator call validator with correct email

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -13,18 +13,20 @@ const makeEmailValidator = () => {
 describe('Email Validator', () => {
   test('should return true if validator returns true', () => {
     const emailValidator = makeEmailValidator()
-
     const isEmailValid = emailValidator.isValid('valid_email@mail.com')
-
     expect(isEmailValid).toBe(true)
   })
 
   test('should return false if validator returns false', () => {
     validator.isEmailValid = false
     const emailValidator = makeEmailValidator()
-
     const isEmailValid = emailValidator.isValid('invalid_email@mail.com')
-
     expect(isEmailValid).toBe(false)
+  })
+
+  test('should call validator with correct email', () => {
+    const emailValidator = makeEmailValidator()
+    emailValidator.isValid('any_email@mail.com')
+    expect(validator.email).toBe('any_email@mail.com')
   })
 })
